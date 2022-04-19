7f3e218b48d6546aa5551a234a8f22966a096784
edwinsatya <edwinsatyayudistira@gmail.com>
2022-04-19 21:04:08 +0700

    setup next, tailwind, and apollo-client

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,76 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useQuery, gql } from "@apollo/client";
+
+const pokemons = gql`
+  query pokemons($limit: Int, $offset: Int) {
+    pokemons(limit: $limit, offset: $offset) {
+      next
+      previous
+      message
+      results {
+        id
+        url
+        name
+        image
+      }
+    }
+  }
+`;
+
+const pokemon = gql`
+  query pokemon($name: String!) {
+    pokemon(name: $name) {
+      id
+      name
+      sprites {
+        front_default
+      }
+      moves {
+        move {
+          name
+        }
+      }
+      types {
+        type {
+          name
+        }
+      }
+    }
+  }
+`;
 
 export default function Home() {
+  // const variables = {
+  //   limit: 12,
+  //   offset: 0,
+  // };
+
+  // const { data, error, loading } = useQuery(pokemons, {
+  //   variables,
+  // });
+
+  const variables = {
+    name: "bulbasaur",
+  };
+
+  const { data, error, loading } = useQuery(pokemon, {
+    variables,
+  });
+
+  if (error) {
+    console.log(error);
+  }
+
+  if (loading) {
+    console.log("loading");
+  }
+
+  if (data) {
+    console.log(data);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,59 +79,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
     </div>
-  )
+  );
 }
