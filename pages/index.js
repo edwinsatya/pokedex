ce28ddfedd780c6fb488ace87baf521205240cb8
edwinsatya <edwinsatyayudistira@gmail.com>
2022-04-20 02:41:11 +0700

    test using graphql in home page for list all pokemon using fetchMore

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,53 @@
 import Layout from "../components/layout/Layout";
 import { useQuery } from "@apollo/client";
-import { GET_POKEMON } from "../graphQl/query";
+import { GET_POKEMONS } from "../graphQl/queries";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  // const variables = {
-  //   limit: 12,
-  //   offset: 0,
-  // };
-
-  // const { data, error, loading } = useQuery(pokemons, {
-  //   variables,
-  // });
+  const [listPokemon, setListPokemon] = useState([]);
+  const [offset, setOffset] = useState(0);
 
   const variables = {
-    name: "bulbasaur",
+    limit: 12,
+    offset,
   };
 
-  const { data, error, loading } = useQuery(GET_POKEMON, {
+  const { data, error, loading, fetchMore } = useQuery(GET_POKEMONS, {
     variables,
   });
+
+  const handleShowMore = () => {
+    const offsetLimit = data.pokemons.next.split("?")[1].split("&");
+    const offset = parseInt(offsetLimit[0].split("=")[1]);
+    // const limit = parseInt(offsetLimit[1].split("=")[1]);
+    fetchMore({
+      variables: {
+        limit: 12,
+        offset,
+      },
+    });
+  };
+
+  useEffect(() => {
+    console.log("trigger");
+    if (data) {
+      let newListPokemon = [...listPokemon, ...data.pokemons.results];
+      console.log(data);
+      setListPokemon(newListPokemon);
+
+      // setVariables({
+      //   limit,
+      //   offset,
+      // });
+    }
+  }, [data]);
 
   if (error) {
     console.log(error);
   }
 
   if (loading) {
-    console.log("loading");
-  }
-
-  if (data) {
-    console.log(data);
+    return <p>loading..</p>;
   }
 
   return (
@@ -37,6 +55,12 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline text-red-500">
         Hello world!
       </h1>
+      <div>
+        {listPokemon.map((pokemon) => (
+          <p key={pokemon.id}>{pokemon.name}</p>
+        ))}
+      </div>
+      <button onClick={() => handleShowMore()}>show more</button>
     </Layout>
   );
 }
