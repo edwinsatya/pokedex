1a851a71ade268426738419e329bd36098ecbb10
edwinsatya <edwinsatyayudistira@gmail.com>
2022-04-20 00:13:39 +0700

    update component header for responsive design

--- components/header/Header.js
+++ components/header/Header.js
@@ -4,11 +4,17 @@ import Menu from "../buttons/Menu";
 
 export default function Header() {
   return (
-    <header className="flex py-2 px-5">
-      <div className="relative flex justify-center items-center w-full">
+    <header className="flex py-2 px-5 md:px-12">
+      <div className="relative flex justify-center items-center w-full md:justify-start">
         <Menu className="absolute flex items-center justify-center w-7 h-5 left-0 md:hidden" />
-        <div className="block w-28 h-10">
-          <Image src={logoImg} alt="logo" layout={"responsive"} priority />
+        <div className="relative w-28 h-10 md:w-40 md:h-14">
+          <Image
+            src={logoImg}
+            alt="logo"
+            layout={"fill"}
+            priority
+            quality={100}
+          />
         </div>
       </div>
     </header>
