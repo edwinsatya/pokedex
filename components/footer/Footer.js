4d98ff12cdd21275e4e5108b559123c25c1c79ea
edwin satya <edwinsatyayudistira@gmail.com>
2022-04-24 12:02:32 +0700

    update font size in footer component

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="text-black font-bold text-xl text-center bg-white w-full p-4">
+    <div className="text-black font-bold text-xs md:text-xl text-center bg-white w-full p-4">
       Copyright &copy; {new Date().getFullYear()} pokedex.edwinsatya.com | All
       rights reserved.
     </div>
